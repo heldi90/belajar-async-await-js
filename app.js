387fd736dd5b670d99fe0edc85a99d87ca082d8f
Heldi Arisandi <heldibisnis@gmail.com>
2021-12-08 21:43:47 +0800

    berhasil menguasai asyn await

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('Hello Word');
+const doSomethingAsync = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('I did something'), 3000);
+  });
+};
+
+const detik = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('menunggu '), 1000);
+  });
+};
+
+const doSomething = async () => {
+  tes = 0;
+  while (true) {
+    if (tes < 10) {
+      console.log(await detik(), tes + 1);
+      tes++;
+    } else {
+      console.log(await doSomethingAsync());
+      break;
+    }
+  }
+};
+
+const setelah = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('Selesai juga'), 15000);
+  });
+};
+
+const after = async (nama) => {
+  console.log(await setelah(), nama);
+};
+
+console.log('Before');
+doSomething();
+after('Heldi');
