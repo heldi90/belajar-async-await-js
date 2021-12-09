d703c5ecbb69fe77fcb4d37f01f733e7e58c5b2a
Heldi Arisandi <heldibisnis@gmail.com>
2021-12-09 22:01:18 +0800

    melatih fecth dan aycn await

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
-const doSomethingAsync = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve('I did something'), 3000);
-  });
+async function hello() {
+  let a = await Promise.resolve('heldi');
+  return a;
+}
+
+const a = async () => {
+  let b = await hello();
+  console.log(b);
 };
 
-const detik = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve('menunggu '), 1000);
-  });
-};
-
-const doSomething = async () => {
-  tes = 0;
-  while (true) {
-    if (tes < 10) {
-      console.log(await detik(), tes + 1);
-      tes++;
-    } else {
-      console.log(await doSomethingAsync());
-      break;
-    }
-  }
-};
-
-const setelah = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve('Selesai juga'), 15000);
-  });
-};
-
-const after = async (nama) => {
-  console.log(await setelah(), nama);
-};
-
-console.log('Before');
-doSomething();
-after('Heldi');
+a();
